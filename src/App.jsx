5d68b186d31a6a67a61a29a66936aba8d5d6b36e
sethzy <limzheyi1996@gmail.com>
2024-12-09 11:22:45 +0800

    1.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,54 @@
 import { useState } from 'react'
-import Button from './button'
 
-const StatisticLine = ({text,value}) => {
-  return <tr><td>{text}</td><td>{value}{text === "positive" ? "%" : ""}</td></tr>
+const AnecdoteOfTheDay = ({text, votes}) => {
+  return <>
+    <h1>Anecdote of the day</h1>
+    <p>{text}</p>
+    <p> has {votes} votes</p>
+  </>
 }
 
-const Statistics = ({average,positive,allClicks,good,neutral,bad}) => {
-  if (allClicks === 0) {
-    return <p>No feedback given</p> 
-  } else {
-    return (
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />  
-          <StatisticLine text ="all" value={allClicks} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positive} />
-        </tbody>
-      </table>
-    )
-  }
+const AnecdoateMostVotes = ({anecdotes, votes}) => {
+  const maxVotes = Math.max(...votes)
+  const indexOfMaxVotes = votes.indexOf(maxVotes)
+  return <>
+    <h1>Anecdote with most votes</h1>
+    <p>{anecdotes[indexOfMaxVotes]}</p>
+    <p> has {votes[indexOfMaxVotes]} votes</p>
+  </>
 }
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const header = 'give feedback'
-  const header2 = 'statistics'
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
-  // save clicks of each Button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allClicks, setallClicks] = useState(0)
-
-  // calculate average and positive
-  let average = allClicks === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / allClicks
-  let positive = allClicks === 0 ? 0 : (good / allClicks) * 100
-
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    setallClicks(allClicks + 1)
-
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    setallClicks(allClicks + 1)
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    setallClicks(allClicks + 1)
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
 
   return (
     <div>
-      <h1>{header}</h1>
-      <Button onClick={handleGoodClick} text="good"/>
-      <Button onClick={handleNeutralClick} text="neutral"/>
-      <Button onClick={handleBadClick} text="bad"/>
-      <h1>{header2}</h1>
-      <Statistics allClicks={allClicks} average={average} positive={positive} good={good} neutral={neutral} bad={bad}/>
+      <AnecdoteOfTheDay text={anecdotes[selected]} votes={votes[selected]} />
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <AnecdoateMostVotes anecdotes={anecdotes} votes={votes} />
     </div>
   )
 }
